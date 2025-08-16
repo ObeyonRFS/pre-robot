b2428e6050ec0f7a9908644a222b752bfbe26561
AdroitBit <yanothaic15@gmail.com>
2025-08-16 21:01:28 +0700

    update directory

--- robot (RPi)/MotorController_uploader.py
+++ robot (RPi)/MotorController_uploader.py
@@ -5,6 +5,10 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Change working directory to that
 os.chdir(script_dir)
-os.chdir("MotorController")
-os.chdir("build/esp32.esp32.esp32da")
-os.system("esptool --chip esp32 --port /dev/ttyUSB1 --baud 921600 write_flash -z 0x1000 .\MotorController.ino.bootloader.bin 0x8000 .\MotorController.ino.partitions.bin 0x10000 .\MotorController.ino.bin")
+os.chdir("./MotorController")
+os.chdir("./build/esp32.esp32.esp32da")
+
+print("Current Directory: ")
+print(os.getcwd())
+print()
+os.system("esptool --chip esp32 --port /dev/ttyUSB1 --baud 921600 write-flash -z 0x1000 .\MotorController.ino.bootloader.bin 0x8000 .\MotorController.ino.partitions.bin 0x10000 .\MotorController.ino.bin")
