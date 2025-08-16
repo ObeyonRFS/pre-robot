fc05bb3bf1775721da03a84db4c7c389bc2b25ef
AdroitBit <yanothaic15@gmail.com>
2025-08-16 20:31:47 +0700

    update

---
+++ robot (RPi)/MotorController/MotorController.ino
@@ -0,0 +1,20 @@
+// Define LED pin
+
+#define LED_PIN 2   // On-board LED is usually GPIO2
+
+
+
+void setup() {
+  // Set pin as output
+}
+  pinMode(LED_PIN, OUTPUT);
+void loop() {
+
+  // Turn LED on
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000); // wait 1 second
+
+  // Turn LED off
+  digitalWrite(LED_PIN, LOW);
+  delay(1000); // wait 1 second
+}
