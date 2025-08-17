daf37483c55b9fe51459ff70e7c4666c582449a0
AdroitBit <yanothaic15@gmail.com>
2025-08-17 16:10:09 +0700

    test rotation

---
+++ barebone (no ROS, no Obeyon)/robot (RPi)/MotorController_uploader.py
@@ -0,0 +1,14 @@
+import os
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Change working directory to that
+os.chdir(script_dir)
+os.chdir("./MotorController")
+os.chdir("./build/esp32.esp32.esp32da")
+
+print("Current Directory: ")
+print(os.getcwd())
+print()
+os.system("esptool --chip esp32 --port /dev/ttyUSB1 --baud 921600 write-flash -z 0x1000 ./MotorController.ino.bootloader.bin 0x8000 ./MotorController.ino.partitions.bin 0x10000 ./MotorController.ino.bin")
